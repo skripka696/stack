eedeaeeacda12a50feb24b2a41ccceda9909b5ed
TurboBelka <turbo.belka@bk.ru>
2017-02-02 09:28:00 +0200

    users model and registration frontend

---
+++ frontend/angular2-webpack-starter/src/app/app.component.ts
@@ -0,0 +1,68 @@
+/*
+ * Angular 2 decorators and services
+ */
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+import { AppState } from './app.service';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.component.css'
+  ],
+  template: `
+    <div class="container">
+      <nav>       
+        <a [routerLink]=" ['./home'] " routerLinkActive="active">
+          Home
+        </a>
+        <a [routerLink]=" ['./login'] " routerLinkActive="active">
+          Login
+        </a>     
+        <a [routerLink]=" ['./registration'] " routerLinkActive="active">
+          Registration
+        </a> 
+      </nav>
+
+      <main>
+        <router-outlet></router-outlet>
+      </main>
+    </div>
+
+    <footer class="footer">
+      <div class="container">
+        <p>@OurStackoverflow 2017</p>
+      </div>
+    </footer>
+  `
+})
+export class AppComponent implements OnInit {
+  public angularclassLogo = 'assets/img/angularclass-avatar.png';
+  public name = 'Angular 2 Webpack Starter';
+  public url = 'https://twitter.com/AngularClass';
+
+  constructor(
+    public appState: AppState
+  ) {}
+
+  public ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
+
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
---
+++ frontend/angular2-webpack-starter/src/app/models/user.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {User} from './user.model';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class UserService{
+	user: User;
+
+	constructor(private http: Http){}
+
+	private jwt(): RequestOptions{
+		// create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
+            return new RequestOptions({ headers: headers });
+        }
+	}
+
+	createNewUser(newUser: User): Observable<User>{
+		console.log('New User');
+		console.log(newUser);
+		return this.http.post('/api/users', newUser, this.jwt()).map((response: Response) => response.json());
+	}
+}
