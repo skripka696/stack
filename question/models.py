024bd7c1d2320baffc6c1ef52cddc694157bff0d
Julia <july.skripka@gmail.com>
2017-02-09 12:05:39 +0200

    add stack.ini file and some changes in models

--- question/models.py
+++ question/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from tag.models import Tag
-from user_profile.models import User
-from stack.settings import AUTH_USER_MODEL
 from django.conf import settings
 
 
@@ -19,7 +17,7 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
-    user = models.ForeignKey(AUTH_USER_MODEL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     question = models.ForeignKey(Question)
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -30,7 +28,7 @@ class Answer(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(AUTH_USER_MODEL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     like = models.IntegerField(default=0)
     description = models.TextField()
     content_type = models.ForeignKey(ContentType)
@@ -42,7 +40,7 @@ class Comment(models.Model):
 
 
 class Vote(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     rating = models.IntegerField()
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
@@ -52,10 +50,3 @@ class Vote(models.Model):
         return '{}'.format(self.rating)
 
 
-# def count_vote(self, val, object_id):
-#     """ val -  """
-#     v = Vote.objects.get(object_id=object_id)
-#     rating = v.rating+val
-#     v.save()
-#
-#     return rating
