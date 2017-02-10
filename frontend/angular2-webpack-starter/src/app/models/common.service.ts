dd1bcc94eeff90c05bb16b4d2379d83a0198fa8b
TurboBelka <turbo.belka@bk.ru>
2017-02-10 16:13:04 +0200

    create common service

---
+++ frontend/angular2-webpack-starter/src/app/models/common.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class CommonService{
+	// serverName: string = 'http://172.16.205.7';
+	serverName: string = 'http://stackoverflow.loc';
+	public http: Http;
+
+	constructor(){}
+
+	public jwt(): RequestOptions{
+		// create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
+            return new RequestOptions({ headers: headers });
+        }
+	}
+
+	public getCookie(name) {
+	    let value = "; " + document.cookie;
+	    let parts = value.split("; " + name + "=");
+	    if (parts.length == 2)
+	      return parts.pop().split(";").shift();
+	}
+
+	public getCSRFToken(){
+		return new Headers({
+            'Content-Type': 'application/json',
+            'X-CSRFToken': this.getCookie('csrftoken')
+        });
+	}
+}
--- frontend/angular2-webpack-starter/src/app/models/user.service.ts
+++ frontend/angular2-webpack-starter/src/app/models/user.service.ts
@@ -1,51 +1,29 @@
 import { Injectable } from '@angular/core';
 import { User } from './user.model';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Http, RequestOptions, Response } from '@angular/http';
+import { CommonService } from './common.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
-export class UserService{
+export class UserService extends CommonService{
 	user: User;
-	// serverName: string = 'http://172.16.205.7';
-	serverName: string = 'http://stackoverflow.loc';
+    options: RequestOptions;
 
-	constructor(private http: Http){}
-
-	private jwt(): RequestOptions{
-		// create authorization header with jwt token
-        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
-            return new RequestOptions({ headers: headers });
-        }
-	}
-
-	getCookie(name) {
-	    let value = "; " + document.cookie;
-	    let parts = value.split("; " + name + "=");
-	    if (parts.length == 2)
-	      return parts.pop().split(";").shift();
-	}
+	constructor(){
+		super();
+		this.options 
+	}	
 
 	createNewUser(newUser: User): Observable<Response>{
-		let headers = new Headers({
-            'Content-Type': 'application/json',
-            'X-CSRFToken': this.getCookie('csrftoken')
-        });
-
-        let options = new RequestOptions({ headers: headers });
+        let options = new RequestOptions({ headers: this.getCSRFToken() });
 		return this.http.post(`${this.serverName}/api/users/`, newUser, options)
 							.map((response: Response) => response);
 	}
 
 	loginUser(user: User){
-		let headers = new Headers({
-            'Content-Type': 'application/json',
-            'X-CSRFToken': this.getCookie('csrftoken')
-        });
-		let options = new RequestOptions({ headers: headers });
+		let options = new RequestOptions({ headers: this.getCSRFToken() });
 		return this.http.post(`${this.serverName}/api-token-auth/`, JSON.stringify(user), options)
 							 .map((response: Response) => response.json());
 	}
