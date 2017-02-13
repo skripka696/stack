edae0fd395740cf463b6e0123a8b4a65317b23ea
Julia <july.skripka@gmail.com>
2017-02-13 10:01:09 +0200

    added  check for expired date for update

--- question/views.py
+++ question/views.py
@@ -57,18 +57,23 @@ class VoteView(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    # def update(self, request, *args, **kwargs):
-    #     partial = kwargs.pop('partial', False)
-    #     instance = self.get_object()
-    #     serializer = self.get_serializer(instance, data=request.data, partial=partial)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_update(serializer)
-    #
-    #     if getattr(instance, '_prefetched_objects_cache', None):
-    #         # If 'prefetch_related' has been applied to a queryset, we need to
-    #         # refresh the instance from the database.
-    #         instance = self.get_object()
-    #         serializer = self.get_serializer(instance)
+    def update(self, request, *args, **kwargs):
+        change_model = ContentType.objects.get(id=request.data['content_type'])
+        data = change_model.model_class().objects.get(id=request.data['object_id']).create_date
+        diff_date = datetime.datetime.now() - data
+        if diff_date.hour > 3:
+            return Response({'error': 'EXPIRED FOR UPDATE VOTING'}, status=status.HTTP_400_BAD_REQUEST)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # refresh the instance from the database.
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
 
         return Response(serializer.data)
 
